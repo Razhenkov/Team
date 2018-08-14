a432ce88c1d1f571172e371d7e0bff751771884c
Razhenkov <blagovest@23snaps.com>
2018-08-14 23:12:08 +0300

    Init - created database and web projects

---
+++ Team.Database/dbo/Tables/Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]           BIGINT         IDENTITY (1, 1) NOT NULL,
+    [FirstName]    NVARCHAR (25)  NOT NULL,
+    [LastName]     NVARCHAR (25)  NOT NULL,
+    [EmailAddress] VARCHAR (120)  NOT NULL,
+    [Password]     NVARCHAR (100) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
